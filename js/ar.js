8b454f1099e99d2bb2aceb04caf3a8390dabf112
masahiro8 <masahiro819@gmail.com>
2021-02-17 20:25:54 +0900

    entityでvideo再生

--- js/ar.js
+++ js/ar.js
@@ -1,12 +1,11 @@
 window.onload = function () {
   AFRAME.registerComponent("videohandler", {
     init: function () {
-      const vid = document.querySelector("#vid");
       const marker = this.el;
-
       marker.addEventListener(
         "markerFound",
         function () {
+          const vid = document.querySelector("#vid");
           vid.play();
         }.bind(this)
       );
@@ -14,6 +13,7 @@ window.onload = function () {
       marker.addEventListener(
         "markerLost",
         function () {
+          const vid = document.querySelector("#vid");
           vid.pause();
           vid.currentTime = 0;
         }.bind(this)
