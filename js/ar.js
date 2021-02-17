5b3509d7bf03281fef8a5291a3298a9b30ff3f70
masahiro8 <masahiro819@gmail.com>
2021-02-17 20:51:57 +0900

    メッセージを追加

--- js/ar.js
+++ js/ar.js
@@ -2,13 +2,16 @@ window.onload = function () {
   AFRAME.registerComponent("videohandler", {
     init: function () {
       const vid = document.querySelector("#vid");
+      const message = document.querySelector("#message");
       const marker = this.el;
 
       marker.addEventListener("markerFound", function () {
+        message.innerHTML = "<p>Found</p>";
         vid.play();
       });
 
       marker.addEventListener("markerLost", function () {
+        message.innerHTML = "<p>Lost</p>";
         vid.pause();
         vid.currentTime = 0;
       });
