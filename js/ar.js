b7503f8668ea2b9317d6c6ed5988913639be4a05
masahiro8 <masahiro819@gmail.com>
2021-02-17 20:36:48 +0900

    js修正

--- js/ar.js
+++ js/ar.js
@@ -1,23 +1,17 @@
 window.onload = function () {
   AFRAME.registerComponent("videohandler", {
     init: function () {
+      const vid = document.querySelector("#vid");
       const marker = this.el;
-      marker.addEventListener(
-        "markerFound",
-        function () {
-          const vid = document.querySelector("#vid");
-          vid.play();
-        }.bind(this)
-      );
 
-      marker.addEventListener(
-        "markerLost",
-        function () {
-          const vid = document.querySelector("#vid");
-          vid.pause();
-          vid.currentTime = 0;
-        }.bind(this)
-      );
+      marker.addEventListener("markerFound", function () {
+        vid.play();
+      });
+
+      marker.addEventListener("markerLost", function () {
+        vid.pause();
+        vid.currentTime = 0;
+      });
     },
   });
 };
